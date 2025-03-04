024a65fb5e25b23d42c2466a44880fbbde0b822d
faizanqubaz <faizanquba1@gmail.com>
2025-03-04 22:36:44 +0500

    workkksasaaaaaaccx

--- src/index.ts
+++ src/index.ts
@@ -48,7 +48,7 @@ app.options('*', cors(corsOptions));
 
 // If using cookies or JWT tokens, add this middleware:
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://beware-seven.vercel.app");
+  res.header("Access-Control-Allow-Origin", "https://beware-frontend-d7uq.vercel.app");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
